5bd4494a9410ee384781f801d766b0cd8036e643
Devkota <info@devkotasagar.com.np>
2017-10-17 20:40:31 +0545

    Alter index.html to index.php and connect with database

--- createdb.php
+++ createdb.php
@@ -4,9 +4,9 @@ $sql ="CREATE TABLE enquiry(
   id INT(6) AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(60) NOT NULL,
   email VARCHAR(60) NOT NULL,
-  phone VARCHAR(60),
-  date VARCHAR(60),
-  peoples INT(60),
+  phone VARCHAR(60) NULL,
+  adate VARCHAR(60) NULL,
+  peoples INT(60) NULL,
   message VARCHAR(460) NOT NULL)";
 $conn-> query($sql);
 
